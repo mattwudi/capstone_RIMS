4a57f4a356d4ca01a72549b3f6462f8e6dac1bca
Matt Wudi <mwudi1@student.cvtc.edu>
2022-11-10 20:32:15 -0600

    Removed PORT logging statement

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
