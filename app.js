444d3b2d55457cb004ec7605a91659f722851d81
Matt Wudi <mwudi1@student.cvtc.edu>
2022-11-10 20:06:25 -0600

    Created basic layout template

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
+
+const PORT = process.env.PORT || 5163;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var aboutRouter = require('./routes/about');
 var app = express();
 
 // view engine setup
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +45,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+// .get for various pages
 module.exports = app;
--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET about page
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/about', {title: 'About'})
 });
 
 module.exports = router;
