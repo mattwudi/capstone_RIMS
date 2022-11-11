5acda39a81d4b0b634b34307aac797be3a7decd1
Danial Kappus <dkappus@student.cvtc.edu>
2022-11-10 21:16:29 -0600

    Adding Port Listener to the end of the app.js

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 
 // .get for various pages
 module.exports = app;
